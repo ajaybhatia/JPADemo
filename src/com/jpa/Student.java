52d4377710f03d36e33a0b3ad5a1aa5ddadacc35
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-11-13 12:32:15 +0530

    Java ORM demo using Oracle JPA

---
+++ src/com/jpa/Student.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author ajay
+ */
+@Entity
+public class Student implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String Branch;
+    private Integer rollno;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBranch() {
+        return Branch;
+    }
+
+    public void setBranch(String Branch) {
+        this.Branch = Branch;
+    }
+
+    public Integer getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(Integer rollno) {
+        this.rollno = rollno;
+    }
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Student)) {
+            return false;
+        }
+        Student other = (Student) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.jpa.Student[ id=" + id + " ]";
+    }
+    
+}
